6177c9e0f1dc66716b742894eaf9f76206210d10
walidsaber <walid_saber123@outlook.com>
2023-03-16 09:43:20 -0700

    u

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,4 +13,5 @@ printf("Size of an int: %ld byte(s)\n", sizeof(int));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
+return (0);
 }
