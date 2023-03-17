2a7c24c5c7a31d4ad3dc1073c4894b1dcc865c98
walidsaber <walid_saber123@outlook.com>
2023-03-17 08:41:00 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : "base 16"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+for (n = 0; n <= 16; n++)
+{
+if (n < 10)
+{
+putchar(n + '0');
+	}
+	else
+	{
+	putchar(n - 10 + 'a');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
