8aaa66ee0e764d5e67c731221d74bb0ac465502c
walidsaber <walid_saber123@outlook.com>
2023-03-19 12:59:04 -0700

    longest output?

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: "4 loops to print us two two-digit numbers comb"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k, a;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+		for (k = 48; k <= 57; k++)
+		{
+		for (a = 48; a <= 57; a++)
+		{
+		putchar(i);
+		putchar(j);
+		putchar(' ');
+		putchar(k);
+		putchar(a);
+		if (i + j == 114 && k + a == 114)
+		{
+		}
+		else
+		{
+		putchar(',');
+		}
+		}
+		}
+		}
+	}
+	putchar('\n');
+}
