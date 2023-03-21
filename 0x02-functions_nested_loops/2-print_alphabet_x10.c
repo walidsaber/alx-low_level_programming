ccff316484186bb8eda50d56f12b0ba72cf6f63a
walidsaber <walid_saber123@outlook.com>
2023-03-21 05:00:34 -0700

    10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry
+ * Description: "print alphabet 10 times!"
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int x = 10;
+
+	while (x <= 10)
+	{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	}
+	_putchar('\n');
+
+}
