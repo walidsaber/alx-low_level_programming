0b4dde06dbf22f1b54cad24e935b20e09241bec1
walidsaber <walid_saber123@outlook.com>
2023-03-21 16:32:02 -0700

    battery street the og

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - counting
+ * Description: "prints all natural number from n to 98"
+ * Return: Always 0 (success)
+ */
+void print_to_98(int n)
+{
+int i;
+
+for (i = n; i <= 98; i++)
+{
+if ( i != 98)
+{
+printf("%d, ",i);
+}
+else
+{
+printf("%d",i);
+}
+}
+}
