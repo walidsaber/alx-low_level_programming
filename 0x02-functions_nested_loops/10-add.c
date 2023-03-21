4b154d56e5fb7c1492547af16a6c790610551cf1
walidsaber <walid_saber123@outlook.com>
2023-03-21 15:28:19 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * add - calculation
+ * Description: "adds two integers together"
+ * @y: value2
+ * @x: value1
+ * Return: results
+ */
+int add(int x, int y)
+{
+int result;
+
+result = x + y;
+if (result < 10)
+{
+_putchar('0' + result);
+}
+else
+{
+_putchar('0' + result / 10);
+_putchar('0' + result % 10);
+}
+}
