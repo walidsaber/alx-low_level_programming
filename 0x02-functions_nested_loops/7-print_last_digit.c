123367ce851a00561ef28d9404a51cff931573af
walidsaber <walid_saber123@outlook.com>
2023-03-21 06:41:06 -0700

    updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_last_digit - Last D
- * Description: "prints the last digit of a number"
+ * Description: "prints the last digit of a givin number"
  * @i: input value
  * Return: i
  */
@@ -12,6 +12,6 @@ if (i < 0)
 i = (i * -1);
 }
 i = (i % 10);
-_putchar(i + '0');
+_putchar('0' + i);
 return (i);
 }
