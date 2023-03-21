140dea66ffa2603e2991a72a4830c81550d64363
walidsaber <walid_saber123@outlook.com>
2023-03-21 03:21:02 -0700

    updated

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: "program to print _putchar"
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
