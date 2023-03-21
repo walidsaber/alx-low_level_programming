1d84e646716265852a398f736840554dafd5ee9b
walidsaber <walid_saber123@outlook.com>
2023-03-21 11:55:38 -0700

    testing

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,5 +6,46 @@
  */
 void jack_bauer(void)
 {
+int i, j;
 
+for (i = 0; i < 24; i++)
+{
+for (j = 0; j <= 59; j++)
+{
+if (i <= 9)
+{
+if (j <= 9)
+{
+_putchar('0');
+_putchar('0' + i);
+_putchar(':');
+_putchar('0');
+_putchar('0' + j);
+}
+else
+{
+_putchar('0');
+_putchar('0' + i);
+_putchar(':');
+_putchar('0' + j);
+}
+}
+else
+{
+if (j <= 9)
+{
+_putchar('0' + i);
+_putchar(':');
+_putchar('0');
+_putchar('0' + j);
+}
+else
+{
+_putchar('0' + i);
+_putchar(':');
+_putchar('0' + j);
+}
+}
+}
+}
 }
