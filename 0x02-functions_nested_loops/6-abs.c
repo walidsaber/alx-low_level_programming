7ac9a26c4fb0c7833c221bf34646a0e93a7ea09d
walidsaber <walid_saber123@outlook.com>
2023-03-21 06:09:13 -0700

    absolutly

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - absolutly
+ * Description: "prints the absolute value"
+ * @i: is an input that will be checked
+ * Return: i
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+	i = (i * -1);
+	}
+	_putchar('\n');
+	return (i);
+}
