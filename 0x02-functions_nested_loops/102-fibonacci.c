149e109509a1444cdc7dbf56cc86f9d0d27dab18
walidsaber <walid_saber123@outlook.com>
2023-03-22 10:21:37 -0700

    updated

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,11 +9,12 @@
 int main(void)
 {
 	int i;
-	long a = 0, b = 1, sum;
+	long a = 1, b = 2, sum;
 
 	for (i = 1; i <= 51; i++)
 	{
-		if ( i != 51){
+		if (i != 51)
+		{
 		printf("%ld, ", a);
 		sum = a + b;
 		a = b;
