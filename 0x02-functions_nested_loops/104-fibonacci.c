7301ca95a12e8d03958195bf8594377f4adb91ff
walidsaber <walid_saber123@outlook.com>
2023-03-22 11:11:40 -0700

    final fibo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry
+ * Description: "long long int "
+ * Return: success 0
+ */
+int main(void)
+{
+	int i;
+
+	unsigned long long a = 1, b = 2, sum;
+
+	for (i = 1; i <= 98; i++)
+	{
+		if (i != 98)
+		{
+			printf("%llu, ", a);
+			sum = a + b;
+			a = b;
+			b = sum;
+		} else
+		{
+			printf("%llu\n", a);
+		}
+	}
+return (0);
+}
