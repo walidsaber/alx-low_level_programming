b15f6e9b05314a5fee50c45e16448cb8199383ae
walidsaber <walid_saber123@outlook.com>
2023-03-22 10:06:00 -0700

    fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: "fibonnaci numbers"
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, a = 0, b = 1, sum;
+
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%d ,", a);
+		sum = a + b;
+		a = b;
+		b = sum;
+	}
+	return (0);
+}
