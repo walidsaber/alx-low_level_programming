829786fdb29f3eda708787a2be687c494ff24807
walidsaber <walid_saber123@outlook.com>
2023-03-23 08:49:55 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - checker
+ * Description: "checks for a digit"
+ * Return: 1 if its a digit,otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ( c == i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
