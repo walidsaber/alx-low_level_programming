fd5704c6ceb009b149a1668946feb59bf57ae596
walidsaber <walid_saber123@outlook.com>
2023-03-23 10:27:20 -0700

    more num

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - 0 to 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, x = 0;
+
+	for (i = 0; i <= 10; i++)
+	{
+	while (x <= 14)
+	{
+		if ( x <= 9)
+		{
+			_putchar('0' + x);
+			x++;
+		}
+		else
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			x++;
+		}
+	}
+	_putchar('\n');
+	x = 0;
+	}
+}
