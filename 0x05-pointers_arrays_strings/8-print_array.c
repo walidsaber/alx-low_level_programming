b9092b60fdbb4b45c409bf3813c6fae9a2b0cbb6
walidsaber <walid_saber123@outlook.com>
2023-03-27 10:09:52 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_array - array
+ * @a: arr
+ * @n: length
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (n != '\0')
+	{
+		if (n > 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+		i++;
+		n--;
+	}
+}
+
