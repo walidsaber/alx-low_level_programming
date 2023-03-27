fd65307afdcda9a2b4be068f2604190c06a8cf0e
walidsaber <walid_saber123@outlook.com>
2023-03-27 07:20:16 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - rev
+ * @s: char
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0, count = 0;
+
+	while (s[i])
+	{
+		count++;
+		i++;
+	}
+	count--;
+	while (count)
+	{
+		_putchar(s[count]);
+		count--;
+	}
+	_putchar(s[0]);
+}
+
