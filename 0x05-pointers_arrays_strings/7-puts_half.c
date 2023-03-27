8b5fcd2e0210735042d6b0d748bfc9d92a2ee4e8
walidsaber <walid_saber123@outlook.com>
2023-03-27 09:02:48 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - half
+ * @str: var
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, count = 0, l = 0;
+
+	while (str[i])
+	{
+		count++;
+		l++;
+		i++;
+	}
+	count /= 2;
+	for (i = count; i < l; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
