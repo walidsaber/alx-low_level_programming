cb5316bc55c6388da64231645c2a177554175744
walidsaber <walid_saber123@outlook.com>
2023-03-30 18:22:05 -0700

    top

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - main
+ * @str: string
+ * Return: void
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		i++;
+	}
+
+	return (str);
+}
