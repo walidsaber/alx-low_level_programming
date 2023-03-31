c8ec2aa3aedab58cc9909e8bd10a7ad852bb16e5
walidsaber <walid_saber123@outlook.com>
2023-03-31 09:43:26 -0700

    printf

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * void print_number - printf
+ * @n: num
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if ((n / 10) != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n / 10) + '0');
+}
