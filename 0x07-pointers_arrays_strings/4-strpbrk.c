e3b4d849adc941bde0d6a61dc2fca1980e421b69
walidsaber <walid_saber123@outlook.com>
2023-04-03 15:09:22 -0700

    srch

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - srch
+ * @s: char
+ * @accept: char
+ * Return: byt
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+
+		s++;
+	}
+	return ('\0');
+}
