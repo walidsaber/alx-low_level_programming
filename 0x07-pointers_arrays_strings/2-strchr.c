7ba8fa1e935fb8c3e9d7a5aac23fd71ab9c5c8c3
walidsaber <walid_saber123@outlook.com>
2023-04-03 15:00:39 -0700

    loc

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i])
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-		{
 			return (s + i);
-		}
-		i++;
 	}
+
 
 	return ('\0');
 }
