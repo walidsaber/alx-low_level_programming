49e5e3969f538d841c79362e448f3782ad5e1b6c
walidsaber <walid_saber123@outlook.com>
2023-04-03 14:32:29 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memset - memsetf
+ * @s: char
+ * @b: char
+ * @n: int
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+}
