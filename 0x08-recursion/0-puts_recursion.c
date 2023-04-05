702a6aae303dde69443260332add68a194ffef0d
walidsaber <walid_saber123@outlook.com>
2023-04-05 14:36:23 -0700

    puts rec

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		putchar(*s);
-		puts(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 }
