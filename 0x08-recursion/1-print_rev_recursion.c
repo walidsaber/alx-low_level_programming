3d0db51d8192ba40fa9fbc7bb2549abb55f1ab3f
walidsaber <walid_saber123@outlook.com>
2023-04-05 14:42:18 -0700

    rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - put rev
+ * @s: char
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}
+
