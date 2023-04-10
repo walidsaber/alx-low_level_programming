058109e31563dfd721e02befd20607ffb0e4e2c8
walidsaber <walid_saber123@outlook.com>
2023-04-10 07:18:46 -0700

    coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <ctype.h>
+#include <string.h>
 
 /**
  * main - change
@@ -16,36 +18,37 @@ int main(int argc, char *argv[])
 	if (argc == 2)
 	{
 		num = atoi(argv[1]);
-		while (num != 0)
+		if (num >= 0)
 		{
-			if (num >= 25)
+			while (num != 0)
 			{
-				num = num - 25;
+				if (num >= 25)
+				{
+					num -= 25;
+				} else if (num >= 10)
+				{
+					num -= 10;
+				} else if (num >= 5)
+				{
+					num -= 5;
+				} else if (num >= 2)
+				{
+					num -= 2;
+				} else
+				{
+					num -= 1;
+				}
 				count++;
-			} else if (num >= 10)
-			{
-				num = num - 10;
-				count++;
-			} else if (num >= 5)
-			{
-				num = num - 5;
-				count++;
-			} else if (num >= 2)
-			{
-				num = num - 2;
-				count++;
-			} else
-			{
-			num = num - 1;
-			count++;
 			}
+			printf("%d\n", count);
+		} else
+		{
+			printf("0\n");
+			return (1);
 		}
-		printf("%d\n", count);
-	}
-	else
+	} else
 	{
 		printf("Error\n");
-		return (1);
 	}
 	return (0);
 }
