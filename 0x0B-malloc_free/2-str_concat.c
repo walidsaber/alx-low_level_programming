f34b248e9532142fa19a8b262f7877dc33512dca
walidsaber <walid_saber123@outlook.com>
2023-04-11 11:00:31 -0700

    concacts

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,13 +14,15 @@ char *str_concat(char *s1, char *s2)
 
 	char *sum;
 
-	if (s1 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		s1 = "";
-	} else if (s2 == NULL)
-	{
 		s2 = "";
 	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i] != '\0')
 	{
 		counts1++;
@@ -32,17 +34,12 @@ char *str_concat(char *s1, char *s2)
 		counts2++;
 		i++;
 	}
-
 	mem = counts2 + counts1;
-	sum = malloc(sizeof(char) * (mem + 1));
+	sum = malloc(sizeof(char) * mem);
 	if (sum == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i <= counts1; i++)
-	{
 		sum[i] = s1[i];
-	}
 	for (i = 0; i <= counts2; i++)
 	{
 		sum[counts1] = s2[i];
