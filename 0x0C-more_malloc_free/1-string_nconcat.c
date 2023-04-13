e095b8b7df206f145e0993d8636cabf833b01d2a
walidsaber <walid_saber123@outlook.com>
2023-04-13 14:27:41 -0700

    best malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,17 +33,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
-	sum = malloc(size1 + size2 + 1);
-
+	sum = malloc(size1 + n + 1);
+	if (sum == NULL)
+		return (NULL);
 	for (i = 0; i < size1; i++)
 	{
 		sum[i] = s1[i];
 	}
-	n = size1 + n;
+	if (n >= size2)
+	{
+		n = size2;
+	} else
+	{
+		n = size1 + n;
+	}
 	for (i = size1; i < n; i++)
 	{
 		sum[i] = s2[k];
 		k++;
 	}
+
 	return (sum);
 }
