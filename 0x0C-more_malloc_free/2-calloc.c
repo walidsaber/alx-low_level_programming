7e68e5d30339817e026d29b66dde9b70a8ad8fd7
walidsaber <walid_saber123@outlook.com>
2023-04-13 16:18:57 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = malloc(size + nmemb);
+	arr = malloc(size * nmemb);
 	if (arr == NULL)
 		return (NULL);
 	c = arr;
