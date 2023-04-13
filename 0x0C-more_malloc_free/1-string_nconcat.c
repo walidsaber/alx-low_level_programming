96b013095d258897314e3c014d4e9d8feda24afc
walidsaber <walid_saber123@outlook.com>
2023-04-13 14:02:21 -0700

    string concacts

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - length
+ * @s: char
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * string_nconcat - concacts
+ * @s1: s1
+ * @s2: s2
+ * @n: n
+ * Return: string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *sum = s1;
+	int i = 0, k = 0, size1 = 0, size2 = 0;
+
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+	sum = malloc(size1 + size2 + 1);
+
+	for (i = 0; i < size1; i++)
+	{
+		sum[i] = s1[i];
+	}
+	n = size1 + n;
+	for (i = size1; i < n; i++)
+	{
+		sum[i] = s2[k];
+		k++;
+	}
+	return (sum);
+}
