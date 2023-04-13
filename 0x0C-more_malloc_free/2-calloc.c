a774670ef19aaa9a576005b576ac4a838b65bf97
walidsaber <walid_saber123@outlook.com>
2023-04-13 16:08:30 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - calloc
+ * @nmemb: int
+ * @size: size
+ * Return: NULL if nmem or size equal to 0.
+ * Otherwise - return arr.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	unsigned int i;
+	char *c;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(size + nmemb);
+	if (arr == NULL)
+		return (NULL);
+	c = arr;
+	for (i = 0; i < size; i++)
+		c[i] = '\0';
+	return (arr);
+}
