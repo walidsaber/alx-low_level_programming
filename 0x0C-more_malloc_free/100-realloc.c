b0c457aeae3b72849ae869d88d4e94f1c3cbd02c
walidsaber <walid_saber123@outlook.com>
2023-04-14 06:35:41 -0700

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - realloc func
+ * @ptr: void
+ * @old_size: old by
+ * @new_size: new by
+ * Return: ptr if new == old, 0 if new_s == 0 and ptr != NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int i;
+	char *ptr2, *data;
+	void *sum;
+
+	if (ptr == NULL)
+	{
+		sum = malloc(new_size);
+		if (sum == NULL)
+			return (NULL);
+		return (sum);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	} else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr2 = ptr;
+	sum = malloc(sizeof(ptr2) * new_size);
+	if (sum == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	data = sum;
+	for (i = 0; i < old_size && i < new_size; i++)
+		data[i] = *ptr2++;
+	free(ptr);
+	return (sum);
+}
