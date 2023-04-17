d85ccf7b5066c951e38505a55281b3f64edb40e4
walidsaber <walid_saber123@outlook.com>
2023-04-17 01:06:54 -0700

    print struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints dog
+ * @d: struct name
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if ((*d).name != NULL)
+	{
+		printf("Name: %s\n", (*d).name);
+	} else
+	{
+		printf("Name: (nil)\n");
+	}
+	if ((*d).age)
+	{
+		printf("Age: %f\n", (*d).age);
+	} else
+	{
+		printf("Age: (nil)\n");
+	}
+	if ((*d).owner != NULL)
+	{
+		printf("Owner: %s\n", (*d).owner);
+	} else 
+	{
+		printf("Owner: (nil)\n");
+	}
+}
