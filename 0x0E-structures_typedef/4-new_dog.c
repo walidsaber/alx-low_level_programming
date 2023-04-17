b5151d631e31a97d1d311f9b6661ba780f8533ab
walidsaber <walid_saber123@outlook.com>
2023-04-17 09:24:43 -0700

    free dogs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * *_strcpy -  cpy
+ * @dest: dest
+ * @src: src
+ * Description: "copy"
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
+/**
  * _strlen - length
  * @s: char
  * Return: length
@@ -28,41 +47,28 @@ int _strlen(char *s)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int size, i;
-	char *n, *o;
+	unsigned int size;
 	dog_t *dogcpy;
 
-	dogcpy = malloc(sizeof(dog_t));
+	dogcpy = malloc(sizeof(dog_t) + 1);
 	if (dogcpy == NULL)
 		return (NULL);
 	size = _strlen(name);
-	dogcpy->name = malloc(sizeof(char) * size);
-	n = malloc(sizeof(char) * size + 1);
+	dogcpy->name = malloc(sizeof(char) * size + 1);
 	if (dogcpy->name == NULL)
 	{
 		free(dogcpy->name);
 		return (NULL);
 	}
 	dogcpy->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	o = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (dogcpy->owner == NULL)
 	{
 		free(dogcpy->owner);
 		free(dogcpy);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-	{
-		n[i] = name[i];
-	}
-	n[size] = '\0';
-	for (i = 0; i < size; i++)
-	{
-		o[i] = owner[i];
-	}
-	o[size] = '\0';
-	dogcpy->name = n;
+	dogcpy->name = _strcpy(dogcpy->name, name);
+	dogcpy->owner = _strcpy(dogcpy->owner, owner);
 	dogcpy->age = age;
-	dogcpy->owner = o;
 	return (dogcpy);
 }
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog
+ * @d: name 
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
