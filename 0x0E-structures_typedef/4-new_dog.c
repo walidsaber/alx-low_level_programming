9585f463c912067235d268910df08ed9dca781d0
walidsaber <walid_saber123@outlook.com>
2023-04-17 09:54:08 -0700

    new d

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -47,27 +47,27 @@ int _strlen(char *s)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *dogcpy;
+	dog_t *dogcpy;
 
-        dogcpy = malloc(sizeof(dog_t));
-        if (dogcpy == NULL)
-                return (NULL);
-        size = _strlen(name);
-        (*dogcpy).name = malloc(sizeof(char) * (_strlen(name) + 1));
-        if ((*dogcpy).name == NULL)
-        {
-                free(dogcpy->name);
-                return (NULL);
-        }
-        (*dogcpy).owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-        if ((*dogcpy).owner == NULL)
-        {
-                free((*dogcpy).owner);
-                free(dogcpy);
-                return (NULL);
-        }
-        (*dogcpy).name = _strcpy(dogcpy->name, name);
-        (*dogcpy).owner = _strcpy(dogcpy->owner, owner);
-        (*dogcpy).age = age;
-        return (dogcpy);
+	dogcpy = malloc(sizeof(dog_t));
+	if (dogcpy == NULL)
+		return (NULL);
+	size = _strlen(name);
+	(*dogcpy).name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dogcpy).name == NULL)
+	{
+		free(dogcpy->name);
+		return (NULL);
+	}
+	(*dogcpy).owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if ((*dogcpy).owner == NULL)
+	{
+		free((*dogcpy).owner);
+		free(dogcpy);
+		return (NULL);
+	}
+	(*dogcpy).name = _strcpy(dogcpy->name, name);
+	(*dogcpy).owner = _strcpy(dogcpy->owner, owner);
+	(*dogcpy).age = age;
+	return (dogcpy);
 }
