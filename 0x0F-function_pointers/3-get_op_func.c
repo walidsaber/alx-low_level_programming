c19863bd3a9c1759cea196bd3be610283d8732fc
walidsaber <walid_saber123@outlook.com>
2023-04-19 08:50:47 -0700

    op func

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,22 @@
+#include "calc.h"
+
+/**
+ * get_op_func - get op
+ * @s: select
+ * Return: result
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+
+	op_t operator[] = {
+		{"+", op_add}, {"-", op_sub},
+		{"*", op_mul}, {"/", op_div},
+		{"%", op_mod},
+		{NULL, '\0'}
+	};
+	while (operator[i].op != '\0' && *(operator[i].op) != *s)
+		i++;
+	return (operator[i].f);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "calc.h"
+
+/**
+ * op_add - add
+ * @a: a
+ * @b: b
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub
+ * @a: a
+ * @b: b
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mul
+ * @a: a
+ * @b: b
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div
+ * @a: a
+ * @b: b
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod
+ * @a: a
+ * @b: b
+ * Return: result
+ */
+int op_mod(int a, int b);
+{
+	return (a % b);
+}
