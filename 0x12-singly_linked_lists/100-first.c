5f3347315cc52abd55f0784ed49aac5aba2aff19
walidsaber <walid_saber123@outlook.com>
2023-04-27 14:01:03 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 
-void my_function() __attribute__ ((constructor));
+void my_function(void) __attribute__ ((constructor));
 /**
  * my_function - executed before main function
  * Return: void
  */
-void my_function() {
-	printf("You're beat! and yet, you must allow,\\nI bore my house upon my back!\\n");
+void my_function(void) {
+	printf("You're beat! and yet, you must allow,"
+			"\nI bore my house upon my back!\n");
 }
