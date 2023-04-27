94479d718c052603cd34df52dfec75d862782c67
walidsaber <walid_saber123@outlook.com>
2023-04-27 10:08:08 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - free list
+ * @head: head of the list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *next_list;
+
+	while (head != NULL)
+	{
+		free_list = head->next;
+		free(head->str);
+		free(head);
+		head = next_list;
+	}
+}
