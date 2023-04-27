99bc074b662a0b07b0e3472002557e322ed48140
walidsaber <walid_saber123@outlook.com>
2023-04-27 10:02:41 -0700

    add node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * len_str - counts len of a string
+ * @n: str
+ * Return: Size
+ */
+int len_str(char *n)
+{
+	int i = 0;
+
+	if (n == NULL)
+		return ('\0');
+	while (n[i])
+		i++;
+	return (i);
+}
+/**
+ * add_node_end - add node at the end of a list
+ * @head: head
+ * @str: string
+ * Return: head
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *add_node, *last_node;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+	add_node = (list_t *)malloc(sizeof(list_t));
+	if (add_node == NULL)
+		return (NULL);
+	add_node->str = strdup(str);
+
+	add_node->len = len_str(add_node->str);
+	if (add_node->str == NULL)
+	{
+		free(add_node);
+		return (NULL);
+	}
+	add_node->next = NULL;
+	if (*head == NULL)
+		*head = add_node;
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+		last_node->next = add_node;
+	}
+	return (*head);
+}
