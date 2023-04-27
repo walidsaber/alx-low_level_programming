16d7a50adbfbb5d53e9621ec628dbf08d2ea206e
walidsaber <walid_saber123@outlook.com>
2023-04-27 09:20:37 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+
+
+/**
+ * len_str - counts len of a string
+ * @n: str
+ * Return: Size
+ */
+int len_str(char *n)
+{
+	int i = 0;
+
+	if (n == NULL)
+		return ('\0');
+	while (n[i])
+		i++;
+	return (i);
+}
 /**
  * add_node - adding node
  * @head: head
@@ -11,16 +30,16 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	char *strcpy;
 	list_t *add_node;
 
-	strcpy = strdup(str);
 	if (head == NULL || str == NULL)
 		return (NULL);
 	add_node = (list_t *)malloc(sizeof(list_t));
 	if (add_node == NULL)
 		return (NULL);
-	add_node->str = strcpy;
+	add_node->str = strdup(str);
+
+	add_node->len = len_str(add_node->str);
 	if (add_node->str == NULL)
 	{
 		free(add_node);
