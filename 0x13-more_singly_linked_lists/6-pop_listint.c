5009f8611438607fbc62162f089f2c197107ae85
walidsaber <walid_saber123@outlook.com>
2023-05-02 11:52:13 -0700

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deleting func
+ * @head: head
+ * Return: data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *headcpy;
+	int data;
+
+	if (*head == NULL)
+		return '\0';
+	headcpy = *head;
+	data = (*head)->n;
+	*head = (*head)->next;
+	free(headcpy);
+	return (data);
+}
