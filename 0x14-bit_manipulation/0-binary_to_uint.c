4ea2c4fb5e40a0b69ac4bd1f001dcc5f4cd91c29
walidsaber <walid_saber123@outlook.com>
2023-05-04 14:32:55 -0700

    binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converter
+ * @b: binary num
+ * Return: 0 if there is one or more chars in b, else return 1
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0, k, multi = 1, sum = 0;
+
+	while (b[i])
+	{
+		if (b[i] != 49 && b[i] != 48)
+			return (0);
+		i++;
+	}
+	for (k = i - 1; k >= 0; k--)
+	{
+		if (b[k] - '0' == 1)
+			sum += multi;
+		multi *= 2;
+	}
+	return (sum);
+}
