6440eb3d72b415d7cc33641b23dd77f396ccdeb1
walidsaber <walid_saber123@outlook.com>
2023-05-04 15:03:49 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary
+ * @n: long int
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int stored = n;
+
+	if (stored > 1 && stored != NULL)
+		print_binary(stored >> 1);
+
+	_putchar((stored & 1) + '0');
+}
