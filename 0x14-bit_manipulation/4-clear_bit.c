dc606d903fdb1a24082f05b0d901756807029849
walidsaber <walid_saber123@outlook.com>
2023-05-05 15:03:37 -0700

    clear

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,7 +8,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index < 0 || index >= sizeof(unsigned int) * 8)
+	if (index >= sizeof(unsigned int) * 8)
 		return (-1);
 	*n &= ~(1 << index);
 	return (1);
