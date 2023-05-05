5fd7941a958776eb3c4c27e0e09beb13a4ec5f5a
walidsaber <walid_saber123@outlook.com>
2023-05-05 13:27:33 -0700

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * get_bit - get bit of given index
+ * @n: num
+ * @index: index
+ * Return: -1 if NULL or error, 0 if success, 1 exit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if ((n & (1 << index)) == 0)
+		return (0);
+	else if (index == NULL)
+		return (-1);
+	return (1);
+}
