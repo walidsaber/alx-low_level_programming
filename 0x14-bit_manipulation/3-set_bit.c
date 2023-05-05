b0df8fe821dd206270ba16fec9c30ebd7203b4e6
walidsaber <walid_saber123@outlook.com>
2023-05-05 14:42:00 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == '\0' || index == '\0')
+	if (index >= sizeof(unsigned int) * 8)
 		return (-1);
 	*n |= (1 << index);
 	return (1);
