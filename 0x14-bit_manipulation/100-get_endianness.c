a5be30cdd5e5c7b36c8034c085e3c1727ed5b812
walidsaber <walid_saber123@outlook.com>
2023-05-05 15:16:58 -0700

    endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - get end
+ * Return: if big - 0, if little - 1.
+ */
+int get_endianness(void)
+{
+	size_t k = 1;
+	char *cr = (char *)&num;
+
+	if (*cr == 1)
+		return (1);
+	return (0);
+}
