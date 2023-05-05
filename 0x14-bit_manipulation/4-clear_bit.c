a56318d2c0fd507161fb326f93f5046c08f8aeb3
walidsaber <walid_saber123@outlook.com>
2023-05-05 15:02:19 -0700

    clear

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * clear_bit - clear bit
+ * @n: n number
+ * @index: at index
+ * Return: 1 if success, else -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < 0 || index >= sizeof(unsigned int) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
