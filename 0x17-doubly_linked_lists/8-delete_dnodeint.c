4aaf066bc6081f1014bf09b3dd5fa9af6636e010
walidsaber <walid_saber123@outlook.com>
2023-06-15 12:45:44 -0700

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,6 +9,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current = *head;
+	unsigned int i;
 
 	if (*head == NULL)
 		return (-1);
@@ -20,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(current);
 		return (1);
 	}
-	for (unsigned int i = 0; i < index; i++)
+	for (i = 0; i < index; i++)
 	{
 		if (current == NULL)
 			return (-1);
---
+++ 0x17-doubly_linked_lists/createfile.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+file_path = "102-result"
+file = open(file_path, "w")
+
+file.write("906609")
+file.close()
