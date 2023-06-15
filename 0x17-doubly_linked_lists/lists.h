a14fd065ee1c06f01e3904e09174ab7c5505267d
walidsaber <walid_saber123@outlook.com>
2023-06-15 06:34:26 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,21 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+#include <stdio.h>
+/**
+ * struct dlistint_s - linked list
+ * @n: int
+ * @prev: pointer to prev
+ * @next: pointer to next
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 #endif
