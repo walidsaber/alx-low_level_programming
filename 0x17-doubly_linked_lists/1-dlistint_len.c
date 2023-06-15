7fc03b75b3dcddd587ee8202747f7bdbb1a25ffc
walidsaber <walid_saber123@outlook.com>
2023-06-15 06:39:13 -0700

    linked len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - print len of a list
+ * @h: pointer
+ * Return: len
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	return(len);
+}
