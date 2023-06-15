2173daa4946c0ceacd45616cc4aa0d9bd604d24f
walidsaber <walid_saber123@outlook.com>
2023-06-15 09:44:28 -0700

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add new node
+ * @head : head
+ * @n : value
+ * Return: new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *addnode;
+
+	addnode = malloc(sizeof(dlistint_t));
+	if (addnode == NULL)
+		return (NULL);
+	if (*head != NULL)
+		(*head)->prev = addnode;
+	addnode->prev = NULL;
+	addnode->n = n;
+	addnode->next = *head;
+	*head = addnode;
+	return (addnode);
+}
