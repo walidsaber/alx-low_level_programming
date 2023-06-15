e920f57bfc00f3418ec6cdc33b83f83800ada5e3
walidsaber <walid_saber123@outlook.com>
2023-06-15 11:40:40 -0700

    6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,16 @@
+/**
+ * sum_dlistint - some of values
+ * @head : head pointer
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	size_t sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
