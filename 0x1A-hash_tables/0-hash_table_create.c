b3b5d5e0cd5ce65e1490ebf60089f6ae858bd6a5
walidsaber <walid_saber123@outlook.com>
2023-07-09 01:49:15 -0700

    g

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	htabl->array = malloc(sizeof(hash_node_t *) * size);
+	htabl->array = malloc(sizeof(hash_node_t *) * size - 1);
 
 	if (htabl->array == NULL)
 	{
