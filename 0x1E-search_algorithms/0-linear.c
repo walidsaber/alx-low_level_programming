d02bbcf6a4fe197b937c5449855eef9c8156706a
walidsaber <walid_saber123@outlook.com>
2023-12-14 10:27:30 -0800

    0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search
+ * @array: int
+ * @size: int
+ * @value: int
+ * Return: result
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!array)
+		return (-1);
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
